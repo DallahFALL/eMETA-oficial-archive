deb66d56724467e956c2ae59304b7adb4144d6ac
DallahFALL <afall644@gmail.com>
2025-05-15 16:57:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.querySelector('.menu-toggle').addEventListener('click', () => {
   document.querySelector('nav').classList.toggle('open');
 });
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    let menu = document.getElementById("menu");
+    let isExpanded = this.getAttribute("aria-expanded") === "true";
+
+    // Inverser l’état
+    this.setAttribute("aria-expanded", !isExpanded);
+    menu.classList.toggle("hidden");
+});
