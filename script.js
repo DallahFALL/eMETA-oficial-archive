bbbb9bc188537194ad8eb05fe5edd2566f35b263
DallahFALL <afall644@gmail.com>
2025-05-18 22:14:38 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var nav = document.querySelector('nav');
-    menuToggle.addEventListener('click', function() {
-        var expanded = this.getAttribute('aria-expanded') === 'true' || false;
-        this.setAttribute('aria-expanded', !expanded);
-        nav.classList.toggle('open');
-    });
-});
