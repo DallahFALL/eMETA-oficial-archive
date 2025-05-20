9643b37f603752c03c4363e516288cb6032563d0
DallahFALL <afall644@gmail.com>
2025-05-20 18:32:33 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function switchLang(lang) {
-  document.querySelectorAll("[data-fr]").forEach(el => {
-    el.textContent = el.getAttribute(`data-${lang}`);
-  });
-}
-window.onload = () => switchLang("fr");
